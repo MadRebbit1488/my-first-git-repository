66631bfeb92e3512676d249e3a308c7bc9f6dc12
MadRebbit1488 <madrebbit@meta.ua>
2023-03-04 21:24:12 +0200

    1

--- if else.js
+++ if else.js
@@ -10,7 +10,85 @@ let hourseCost = 13500;
 
 let res = deposite * (1+rate/12)**24;
 if(res > hourseCost){
-  console.log(`Можемо купити.Залишок ${res-hourseCost}`);
+  console.log(`Ми накопичили ${res}.Можемо купити.Залишок ${res-hourseCost}`);
 }else{
-  console.log('Купити не можемо');
+  console.log(`Ми накопичили ${res}.Купити не можемо`);
+}
+
+
+let secretNumber = '7';
+if(Number(secretNumber) === 7){
+  console.log('Вгадав !!!');
+}else{
+  console.log('Не вгадав');
+}
+
+if(secretNumber == 7){
+  console.log('Вгадав !!!');
+}else{
+  console.log('Не вгадав');
+}
+
+let q = Number(prompt('Введтіть число'));
+if(q === 7){
+  console.log('Вгадав !!!');
+}else{
+  console.log('Не вгадав');
+} 
+
+
+let role = 'manager';
+
+if(role === 'manager'){
+  console.log('Менеджер');
+}else if(role === 'admin') {
+  console.log('Адмін');
+}else if(role === 'seo') {
+  console.log('СЕО');
+}else{
+  console.log('Ми тебе не знаємо');
+}
+
+// switch statement заміняє калька if
+// B switch важливо === рівність, інакше кейс не спрацює 
+
+switch (role) {
+    case 'manager':  //role === 'manager'
+      console.log('Менеджер');
+      break;
+    case 'admin':  //role === 'admin'
+      console.log('Адмін');
+      break;
+    case 'seo':  //role === 'seo'
+      console.log('СЕО');
+      break;
+    default:
+      console.log('Ми тебе не знаємо');
+}
+
+
+switch (role) {
+    case 'manager':  
+    case 'admin':  
+      console.log('Не керівник');
+      break;
+    case 'seo': 
+      console.log('Керівник');
+      break;
+    default:
+      console.log('Ми тебе не знаємо');
+}
+
+let num = 1;
+
+switch (true) {
+    case num > 0:   //true === num > 0
+      console.log('Позитивний');
+      break;
+    case num < 0:
+      console.log('Негативний');
+      break;  
+    default:
+      console.log('Нуль');
+
 }
